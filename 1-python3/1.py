45f0437d964ea099a61530b7f05666c90a31fe73
abeerration <abeerration@gmail.com>
2024-12-03 22:33:28 +0500

    Update 1.py

--- 1-python3/1.py
+++ 1-python3/1.py
@@ -10,3 +10,54 @@ print(type(myInt))
 myFloat = 3.14
 print(type(myFloat))
 
+print("\n------------------\n")
+
+myVariable = 10
+print(type(myVariable))
+myVariable = 10.1
+print(type(myVariable))
+myVariable = "Yes, I can do this"
+print(type(myVariable))
+
+print("\n------------------\n")
+
+A = 1000
+B = A           # B is the LHS and A is the RHS
+print(B)
+C = "My Sentence"
+D = C
+
+print(D)
+# Note we can overwrite the already existing contents of the LHS with assignment
+E = "Pizza"
+F = "Broccoli"
+E = F
+print(E)
+
+A = 10
+B = "Not a number"
+A = B
+print(type(A))
+
+print("\n------------------\n")
+
+print(50+10)
+print(50-10)
+print(50*10)
+print(50/10)
+
+myVariable = 100
+print(-myVariable)
+floatExperiment = myVariable/10
+print(type(floatExperiment))
+
+print(2**10) #Exponentiation
+moduloDivision = 999%100
+print(moduloDivision)
+
+floorDivision = 999//100
+print(floorDivision)
+print(type(floorDivision)) #Floor Division
+
+print("\n------------------\n")
+
